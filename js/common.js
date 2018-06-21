cd0c64f37fec659b68be674b3f39827993fee83d
Bohdan <bohdan.boiko@gmail.com>
2018-06-21 23:48:20 +0300

    Finish PSD to HTML Adaptive

--- js/common.js
+++ js/common.js
@@ -4,5 +4,18 @@ $(document).ready(function(){
     items: 1,
     margin:10,
     nav:true,
-})
+	})
+	console.log('start');
+	$(".navigation__burger").click(function() {
+		if($(".navigation__burger").hasClass("navigation__burger-active"))
+		{
+			$(".navigation__burger").removeClass("navigation__burger-active");
+			$(".navigation__list").removeClass("navigation__list-active");
+		}
+		else{
+			$(".navigation__burger").addClass("navigation__burger-active");
+			$(".navigation__list").addClass("navigation__list-active");
+		}
+
+	});
 });
